8fd22d0d4977754d90a6ed34f6cdd5ca272bd04d
Duilio Flores <duilio.flores@sfact.pe>
2023-10-24 14:15:08 -0500

    actualiz 2do entregable

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,33 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link href="{{ asset('css/styles02.css') }}" rel="stylesheet">
-    <title>Sistema de Matrículas</title>
+
+    <link rel="stylesheet" href="{{ asset('css/styles02.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>@yield('title') SENATI 2023</title>
 </head>
-<body class="d-flex justify-content-center align-items-center vh-100">
-    <div class="text-center">
-        <h1>Iniciar Sesión</h1>
-        <form method="POST" action="{{ route('login.login') }}" class="mb-3">
-            @csrf
-            <div class="form-group">
-                <label class="form-label mt-3" for="email">Correo Electrónico:</label>
-                <input type="text" name="email" id="email" class="form-control" value="">
+<body>
+    
+<div class="container py-4">
+    <div class="row g-0 align-items-center">
+        <div class="col-lg-6 mb-5 mb-lg-0">
+            <div class="card cascading-right" style="background: hsla(0, 0%, 100%, 0.55); backdrop-filter: blur(20px);">
+                <div class="card-body p-5 shadow-5 text-center">
+                    <h2 class="fw-bold mb-5" style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; color: #d9ff00; text-shadow: 3px 3px 7px #000;">Login de Usuario</h2>
+                    <form method="POST" action="{{ route('login.login') }}">
+                        @csrf
+                        <div class="row"> 
+                            <div class="col-md-12">
+                                <div class="form-outline mb-4">
+                                    <label for="email" class="form-label">Correo Electrónico</label>
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese su correo">
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-outline mb-4">
+                                    <label for="contrasena" class="form-label">Contraseña</label>
+                                    <input type="password" class="form-control" id="contrasena" name="contrasena" placeholder="Ingrese su contraseña"/>
+                                </div>
+                            </div>
+                            <div class="form-check d-flex justify-content-center mb-4">
+                                <input class="form-check-input me-2" type="checkbox" value="" id="rememberCheck" name="remember" autocomplete="off"/>
+                                <label class="form-check-label" for="rememberCheck">Mantener sesión iniciada</label>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block mb-4">Acceder</button>
+                    </form>
+                    @if(session('mensaje'))
+                        <p>{{ session('mensaje') }}</p>
+                    @endif
+                </div>
             </div>
-            <div class="form-group">
-                <label class="form-label mt-3" for="contrasena">Contraseña:</label>
-                <input type="password" name="contrasena" id="contrasena" class="form-control" value="">
-            </div>
-            <div class="form-group mt-4">
-                <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-            </div>
-        </form>
-        @if(session('mensaje'))
-            <p>{{ session('mensaje') }}</p>
-        @endif
+        </div>
+        <div class="col-lg-6 mb-5 mb-lg-0">
+            <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" class="w-100 rounded-4 shadow-4" alt="" />
+        </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</div>
+
+<!-- Agrega los scripts de Bootstrap correctamente -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
